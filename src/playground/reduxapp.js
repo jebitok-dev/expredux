9c4d2bc27e0fec4317744093076b6ecae506046b
IjayAbby <inyangasi4@gmail.com>
2020-03-12 12:21:39 +0300

    Using STORE which hold state of an object

--- src/playground/reduxapp.js
+++ src/playground/reduxapp.js
@@ -1,1 +1,41 @@
-console.log("JavaScript is awesome");
+import { createStore } from "redux";
+
+const store = createStore((state = {count : 0} , action) =>{
+    switch (action.type){
+        case "INCREMENT":
+            return {
+                count : state.count + 1
+            }
+            case "DECREMENT" :
+                return {
+                    count : state.count -1
+                }
+                case "RESET":
+                return { count: 0 }
+            default :
+        return state;
+    }
+    
+    
+})
+ 
+store.subscribe((() => {
+    console.log(store.getState());
+}));
+
+
+store.dispatch({
+    type:'INCREMENT',
+    incrementBy : 5
+});
+
+store.dispatch({
+    type:"INCREMENT"
+})
+store.dispatch({
+    type:"DECREMENT"
+})
+
+// store.dispatch({
+//     type:"RESET"
+// });
