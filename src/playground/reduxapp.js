7c00f18fc555931def51b7d7da92b60999b2e82d
IjayAbby <inyangasi4@gmail.com>
2020-03-12 17:05:37 +0300

    Action Creator

--- src/playground/reduxapp.js
+++ src/playground/reduxapp.js
@@ -3,39 +3,65 @@ import { createStore } from "redux";
 const store = createStore((state = {count : 0} , action) =>{
     switch (action.type){
         case "INCREMENT":
+        
             return {
-                count : state.count + 1
+                count : state.count + action.incrementBy
             }
-            case "DECREMENT" :
+        case "DECREMENT" :
+           
                 return {
-                    count : state.count -1
+                    count : state.count - action.decrementBy
                 }
-                case "RESET":
-                return { count: 0 }
+        case "RESET":
+                return {
+                    count : 0
+                }
+        case "SET":
+                return {
+                        count : action.count
+                    }
             default :
         return state;
-    }
-    
-    
+    }   
 })
  
+//Action creator  - functions that return action objects
+const incrementCount = (payload = {}) => {
+    return {
+        type : 'INCREMENT',
+        incrementBy:typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+    }
+}
+const decrementCount = (payload = {}) => {
+    return {
+        type : "DECREMENT",
+        decrementBy : typeof payload.decrementBy === "number" ? payload.decrementBy : 1
+    }
+}
+
+const resetCount = () =>{
+    return {
+        type : 'RESET',
+    }
+}
+
+const setCount = (payload) => {
+    return {
+        type:'SET',
+        count : payload.count
+    }
+}
 store.subscribe((() => {
     console.log(store.getState());
 }));
 
 
-store.dispatch({
-    type:'INCREMENT',
-    incrementBy : 5
-});
+store.dispatch(incrementCount({ incrementBy : 5}));
+store.dispatch(incrementCount())   
+ 
+store.dispatch(resetCount())
 
-store.dispatch({
-    type:"INCREMENT"
-})
-store.dispatch({
-    type:"DECREMENT"
-})
+store.dispatch(decrementCount())
+store.dispatch(decrementCount({decrementBy : 10}))
 
-// store.dispatch({
-//     type:"RESET"
-// });
+store.dispatch(setCount({count : 150}));
